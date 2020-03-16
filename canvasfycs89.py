29fcd1f8f3122f6157ccaf1178090d69233fe2e3
sahil8989 <60725858+sahil8989@users.noreply.github.com>
2020-03-16 11:16:12 +0530

    Update canvasfycs89.py

--- canvasfycs89.py
+++ canvasfycs89.py
@@ -1,17 +1,17 @@
 # Importing tkinter module
 from tkinter import*
 
-root=Tk()
+root = Tk()
 canvas_width = 500
 canvas_height = 500
 # Creating Canvas widget
-c = Canvas(root,width=500,height=500,bg="blue")
+c = Canvas(root, width=500, height=500, bg="blue")
 c.pack()
 # Creating line in Canvas
-y=int(canvas_height/2)
-c.create_line(10,y,canvas_width,y,fill="green")
+y = int(canvas_height/2)
+c.create_line(10, y, canvas_width, y, fill="green")
 # Creating arc and ovalin canvas
-arc=c.create_arc(10,50,240,210,start=90,extent=190,fill="red")
-oval =c.create_oval(50,60,100,100)
+arc = c.create_arc(10, 50, 240, 210, start=90, extent=190, fill="red")
+oval = c.create_oval(50, 60, 100, 100)
 # Attaching  loop to the program
 root.mainloop()
